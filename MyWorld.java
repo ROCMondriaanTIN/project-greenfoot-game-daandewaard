80e4a9cba89842607994b282f4d5ab6e15a16cec
thenorwaygang <44640427+thenorwaygang@users.noreply.github.com>
2018-11-08 14:30:41 +0100

    Locatie enemy aangepast

--- MyWorld.java
+++ MyWorld.java
@@ -51,7 +51,7 @@ public class MyWorld extends World {
         // Alle objecten toevoegen aan de wereld: camera, main karakter en mogelijke enemies
         addObject(camera, 0, 0);
         addObject(hero, 300, 200);
-        addObject(new Enemy(), 1170, 410);
+        addObject(new Enemy(), 1170, 463);
         
         // Initialiseren van de CollisionEngine zodat de speler niet door de tile heen kan lopen.
         // De collision engine kijkt alleen naar de tiles die de variabele solid op true hebben staan.
