8d21f4d7728d6c03a0c9ab5d7c25f17c69f5a758
thenorwaygang <44640427+thenorwaygang@users.noreply.github.com>
2018-11-09 09:25:40 +0100

    Hero valt nu naar beneden en verandert naar een andere image als hij geraakt wordt door een enemy

--- Hero.java
+++ Hero.java
@@ -9,10 +9,11 @@ public class Hero extends Mover {
     private final double gravity;
     private final double acc;
     private final double drag;
+    private  int pause = 30;
 
     public Hero() {
         super();
-        gravity = 9.8;
+        gravity = 2.8;
         acc = 0.61;
         drag = 0.8;
         setImage("p1.png");
@@ -31,6 +32,13 @@ public class Hero extends Mover {
 
         for (Actor enemy : getIntersectingObjects(Enemy.class)) {
             if (enemy != null) {
+                Greenfoot.playSound("death.wav");
+                setImage("p1_hurt.png");
+                if (pause != 0) {
+    pause--;
+    return;
+}
+         
                 getWorld().removeObject(this);
                 break;
             }
