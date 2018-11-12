9d09813a1674791a6c49abf5b5f5b16f09b6751d
thenorwaygang <44640427+thenorwaygang@users.noreply.github.com>
2018-11-12 11:27:53 +0100

    Eerste level toegevoegd

--- Hero.java
+++ Hero.java
@@ -1,4 +1,6 @@
+
 import greenfoot.*;
+
 
 /**
  *
@@ -10,11 +12,16 @@ public class Hero extends Mover {
     private final double acc;
     private final double drag;
     private boolean geraakt = false;
-    private  int pause = 100;
+    private  int pause = 50;
+    private int levens = 3;
+    private boolean canJump = true;
 
+    public void jump(){
+    canJump = false;
+    }
     public Hero() {
         super();
-        gravity = 2.8;
+        gravity = 9.8;
         acc = 0.61;
         drag = 0.8;
         setImage("p1.png");
@@ -22,6 +29,7 @@ public class Hero extends Mover {
 
     @Override
     public void act() {
+        
         handleInput();
         
         velocityX *= drag;
@@ -41,19 +49,27 @@ public class Hero extends Mover {
     pause--;
     return;
 }
+                pause = 50;
          
                 setLocation(300, 200);
                 setImage("p1.png");
             }
             geraakt = false;
-            
+            levens --;
+            System.out.print(levens);
         }
+        
+        if (levens == 0)
+        {
+        getWorld().removeObject(this);
+        
+    }
     }
 
     public void handleInput() {
         if (geraakt == false){
-        if (Greenfoot.isKeyDown("w") && (isTouching(Tile.class))) {
-            velocityY = -15;
+        if (Greenfoot.isKeyDown("w") && (isTouching(Tile.class) && canJump == true)) {
+            velocityY = -25;
         }
 
         if (Greenfoot.isKeyDown("a")) {
