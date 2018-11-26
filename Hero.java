610056390bb5feb8da3fc8f887617ef5f2c68f17
thenorwaygang <44640427+thenorwaygang@users.noreply.github.com>
2018-11-26 10:25:25 +0100

    levelselectie met (tijdelijke) code. wordt later hopelijk vervangen door een levelselectie scherm

--- Hero.java
+++ Hero.java
@@ -162,6 +162,19 @@ public class Hero extends Mover {
                 if (keyCollect == true) {
                     JOptionPane.showMessageDialog(
                             null, "Level voltooid");
+                    if (MyWorld.level == 1)
+                    {
+                        JOptionPane.showMessageDialog(
+                            null, "Level 2 is nu beschikbaar! gebruik de code "
+                                    + "AJ3D9 om het level te spelen");
+                    }
+                    else if (MyWorld.level == 2)
+                    {
+                        JOptionPane.showMessageDialog(
+                            null, "Level 3 is nu beschikbaar! gebruik de code "
+                                    + "D2SNY om het level te spelen");
+                    }
+                    
                     getWorld().removeObject(this);
                 } else if (keyCollect == false) {
 
